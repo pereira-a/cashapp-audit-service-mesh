43b18977bcecffb551bb54c327363f8254aaf8ac
André Pereira <pereirasmandre@gmail.com>
2023-10-06 00:10:03 +0100

    audit apis with gorrila/mux

--- audit-go-lang/audit.go
+++ audit-go-lang/audit.go
@@ -1,27 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
+type Audit struct {
+	Userid    string
+	Operation string
+	Metadata  string
+	Timestamp time.Time
+}
+
 func main() {
 	r := mux.NewRouter()
-
-	r.HandleFunc("/audit", CreateAudit).
+	r.HandleFunc("/audit", GetAudit).
 		Methods("GET").
 		Queries("userid", "{userid}")
 
-	// TODO: api post
+	r.HandleFunc("/audit", CreateAudit).
+		Methods("POST")
 
 	http.ListenAndServe(":8080", r)
 }
 
+func GetAudit(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	audit := Audit{
+		Userid:    "testUser",
+		Operation: "sendMoney",
+		Timestamp: time.Now(),
+	}
+
+	json.NewEncoder(w).Encode(audit)
+}
+
 func CreateAudit(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	user := vars["userid"]
-	fmt.Fprintf(w, "You've requested a GET for userid=%s", user)
+	var user Audit
+	json.NewDecoder(r.Body).Decode(&user)
+	fmt.Fprintf(w, "Received Audit: %s - %s - %s - %s", user.Userid, user.Operation, user.Metadata, user.Timestamp)
 }
